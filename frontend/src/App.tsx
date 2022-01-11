5e37cf3037c25fa3ea25f5e7581ed18fa49c4837
Daniel Pires <pires.daniel@fatec.sp.gov.br>
2022-01-10 22:20:51 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/navbar";
+
+
 function App() {
   return (
-    <h1>Pagina principal</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as Githubicon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar(){
+    return (
+    <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+          <h1>DSMovie</h1>
+          <a className='dsmovie-contact-link' href="https://github.com/pires-daniel">
+          <div className='dsmovie-contact-container'>
+            <Githubicon/>
+            <p>/pires-daniel</p>
+          </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+    );
+}
+
+export default Navbar;
