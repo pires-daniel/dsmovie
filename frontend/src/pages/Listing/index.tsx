cf2f026ca97880ab49a5e3bde33103d41a440d97
Daniel Pires <pires.daniel@fatec.sp.gov.br>
2022-01-11 16:47:02 -0300

    MoviaCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,35 @@
 import Pagination from "components/pagination";
+import MovieCard from "components/pagination/moviecard";
 
 function Listing (){
 
     return (
+    <>
 
         <Pagination />
+
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+            </div>
+        </div>
+
+    </>
+
 
     );
 
