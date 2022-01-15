5b11dfdb11a5010c6b8c4ce3100157793f06a2ff
Daniel Pires <pires.daniel@fatec.sp.gov.br>
2022-01-15 10:57:46 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/dto/ScoreDTO.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsmovie.entities.dto;
+
+public class ScoreDTO {
+	
+	private Long movieid;
+	private String email;
+	private Double score;
+	
+	public ScoreDTO() {
+	
+	}
+	public Long getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(Long movieid) {
+		this.movieid = movieid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+		
+
+}
